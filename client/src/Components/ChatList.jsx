daf2625e06cf7c94f7ee49ae93a3ec32cabf64fc
rupeshk98 <jk429275@gmail.com>
2024-10-14 14:38:24 +0530

    Update ChatList.jsx

--- client/src/Components/ChatList.jsx
+++ client/src/Components/ChatList.jsx
@@ -7,7 +7,7 @@ const ChatList = () => {
   const { isPending, error, data } = useQuery({
     queryKey: ["userChats"],
     queryFn: () =>
-      fetch(`${import.meta.env.VITE_API_URL}/api//userchats`, {
+      fetch(`${import.meta.env.VITE_API_URL}/api/userchats`, {
         credentials: "include",
       }).then((res) => res.json()),
   });
